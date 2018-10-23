f8e1a2ca94667a36a5ac04d87d13e4a3fc3984f1
Jesus Argel <j.argelguerra@gmail.com>
2018-10-23 14:47:01 -0400

    Created variables

--- kiwichallenge/src/kiwichallenge/Main.java
+++ kiwichallenge/src/kiwichallenge/Main.java
@@ -4,6 +4,17 @@ import java.util.*;
 import java.lang.*;
 
 class Main {
+	
+	Scanner sc = new Scanner(System.in);
+	int numOfZones;
+	String departureAirport;
+	String currentAirport;
+	int dayCounter = 0;
+	int totalPrice = 0;
+	Boolean [] zones;
+	String [] routeTaken;
+	int [][] dayOfFlight;
+	int [][] priceOfFlight;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
